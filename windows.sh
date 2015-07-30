5553c88abee9917a35e1ec481ec546dc69d05b05
Osric Wilkinson <osric@fluffypeople.com>
2015-07-30 07:33:35 +0100

    Now using spice

--- windows.sh
+++ windows.sh
@@ -1,4 +1,14 @@
-# sudo qemu-system-x86_64 -m 1024 -hda /dev/mapper/ptah-ptah--win7 -runas osric -enable-kvm -vga std -usbdevice tablet
 
-sudo qemu-system-x86_64 --enable-kvm -hda /dev/mapper/ptah-ptah--win7 -m 2G -rtc base=localtime,clock=host -smp threads=4 -usbdevice tablet -cpu host -vga vmware -netdev bridge,id=hn0 -device e1000,netdev=hn0,id=nic1
+sudo qemu-system-x86_64 \
+	--enable-kvm \
+	-hda /dev/mapper/ptah-ptah--win7 \
+	-m 2.1G \
+	-rtc base=localtime,clock=host \
+	-smp threads=4 \
+	-usbdevice tablet \
+	-cpu host \
+	-vga qxl \
+	-netdev bridge,id=hn0 -device e1000,netdev=hn0,id=nic1 \
+	-spice port=5900,addr=127.0.0.1,disable-ticketing 
+
 
