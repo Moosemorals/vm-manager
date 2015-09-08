ea4dab6481b8670f56aa5da7ca09a8821936592d
Osric Wilkinson <osric@fluffypeople.com>
2015-09-08 06:54:10 +0100

    About to move to a generic script model

--- mafdet.sh
+++ mafdet.sh
@@ -1,3 +1,13 @@
+#/bin/sh
+
+IFACE=tap1
+
+case "$1" in
+	start)
+
+ip tuntap add dev $IFACE mode tap user osric
+ip link set $IFACE up
+brctl addif br0 $IFACE
 
 qemu-system-x86_64 \
 	--enable-kvm \
@@ -9,8 +19,20 @@ qemu-system-x86_64 \
 	-cpu host \
 	-vga none \
 	-display none \
-	-serial tcp:127.0.0.1:5900,server,nowait \
-	-monitor tcp:127.0.0.1:5901,server,nowait \
-	-netdev type=tap,ifname=tap0,script=no,downscript=no,id=net0 \
+	-serial tcp:127.0.0.1:5910,server,nowait \
+	-monitor tcp:127.0.0.1:5911,server,nowait \
+	-netdev type=tap,ifname=$IFACE,script=no,downscript=no,id=net0 \
 	-device virtio-net,netdev=net0,mac=08:00:27:6d:69:5c \
 	-daemonize
+
+	;;
+	stop)
+		echo "q" | nc localhost 5911
+		brctl delif br0 $IFACE
+		ip link set $IFACE down
+		ip tuntap del dev $IFACE mode tap
+	;;
+	*)
+		echo "Usage: $0 {start|stop}"
+		exit 1;
+esac
--- seshat.sh
+++ seshat.sh
@@ -2,14 +2,14 @@
 qemu-system-x86_64 \
 	--enable-kvm \
 	-hda /dev/mapper/ptah-seshat_main \
-	-cdrom /home/osric/ISO/mini.iso \
+	-cdrom /home/osric/ISO/debian-31r8-i386-netinst.iso \
 	-m 2G \
 	-rtc base=localtime,clock=host \
 	-smp threads=4 \
 	-usbdevice tablet \
 	-cpu qemu32 \
-	-vga qxl \
-	-display sdl \
+	-vga none \
+	-display none \
 	-serial tcp:127.0.0.1:5900,server,nowait \
 	-monitor tcp:127.0.0.1:5901,server,nowait \
 	-netdev type=tap,ifname=tap0,script=no,downscript=no,id=net0 \
