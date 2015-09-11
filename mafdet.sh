a7e39d0b03bdf63014795983af4f157cbac4ed38
Osric Wilkinson <osric@fluffypeople.com>
2015-09-11 20:18:34 +0100

    More genercising

--- mafdet.sh
+++ mafdet.sh
@@ -1,33 +1,44 @@
 #/bin/sh
 
-IFACE=tap1
+## Configuration
+
+NAME=mafdet
+ID=1
+
+## Configuration ends
+
+
+IFACE=tap${ID}
+SERIAL=$(expr $ID \* 2)
+MONITOR=$(expr $SERIAL + 5901)
+SERIAL=$(expr $SERIAL + 5900)
 
 case "$1" in
 	start)
 
-ip tuntap add dev $IFACE mode tap user osric
-ip link set $IFACE up
-brctl addif br0 $IFACE
+		ip tuntap add dev $IFACE mode tap user osric
+		ip link set $IFACE up
+		brctl addif br0 $IFACE
 
-qemu-system-x86_64 \
-	--enable-kvm \
-	-hda /dev/mapper/ptah-mafdet_main \
-	-m 2G \
-	-rtc base=localtime,clock=host \
-	-smp threads=4 \
-	-usbdevice tablet \
-	-cpu host \
-	-vga none \
-	-display none \
-	-serial tcp:127.0.0.1:5910,server,nowait \
-	-monitor tcp:127.0.0.1:5911,server,nowait \
-	-netdev type=tap,ifname=$IFACE,script=no,downscript=no,id=net0 \
-	-device virtio-net,netdev=net0,mac=08:00:27:6d:69:5c \
-	-daemonize
+		qemu-system-x86_64 \
+			--enable-kvm \
+			-hda /dev/mapper/ptah-${NAME}_main \
+			-m 2G \
+			-rtc base=localtime,clock=host \
+			-smp threads=4 \
+			-usbdevice tablet \
+			-cpu host \
+			-vga none \
+			-display none \
+			-serial tcp:127.0.0.1:$SERIAL,server,nowait \
+			-monitor tcp:127.0.0.1:$MONITOR,server,nowait \
+			-netdev type=tap,ifname=$IFACE,script=no,downscript=no,id=net0 \
+			-device virtio-net,netdev=net0,mac=08:00:27:6d:69:5c \
+			-daemonize
 
 	;;
 	stop)
-		echo "q" | nc localhost 5911
+		echo "q" | nc localhost $MONITOR 
 		brctl delif br0 $IFACE
 		ip link set $IFACE down
 		ip tuntap del dev $IFACE mode tap
